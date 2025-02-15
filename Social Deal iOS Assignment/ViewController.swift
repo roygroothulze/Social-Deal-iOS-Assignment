1901785cbf08a1cc01eed73ab01ecfc0a3ad6b4d
Roy Groot Hulze <info@roygroothulze.nl>
2025-02-15 19:22:12 +0100

    Initial Commit

---
+++ Social Deal iOS Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Social Deal iOS Assignment
+//
+//  Created by CodeBlock on 15/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
